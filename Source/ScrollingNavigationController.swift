47abce796ffffac8cde0eeb73d541f48e24bb028
Niall Paterson <npaters@tcd.ie>
2015-09-07 18:49:29 +0100

    Update ScrollingNavigationController.swift

--- Source/ScrollingNavigationController.swift
+++ Source/ScrollingNavigationController.swift
@@ -317,9 +317,7 @@ public class ScrollingNavigationController: UINavigationController, UIGestureRec
         // Hide all possible button items and navigation items
         for item in navigationBar.subviews {
             if let view = item as? UIView {
-                if view.classForCoder.description() == "UINavigationButton" || view.classForCoder.description() == "UINavigationItemView" {
-                    view.alpha = alpha
-                }
+                view.alpha = alpha
             }
         }
 
